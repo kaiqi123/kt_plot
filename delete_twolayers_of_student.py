9d321056951afbe5c296a9e06b4057907b1b0ef3
kaiqi123 <kzhao27@asu.edu>
2018-12-13 16:57:08 -0700

    11

--- delete_twolayers_of_student.py
+++ delete_twolayers_of_student.py
@@ -1,9 +1,6 @@
 import matplotlib.pyplot as plt
 from readData import *
 
-# comparing delete and not delete con6 and fc2 of independent student,
-
-
 # original,interval 10
 original_interval_10 = save_data_to_list(read_data("interval/original_interval_10"))
 print("original_interval_10: " + str(caculate_convergence_time(original_interval_10)))
@@ -13,6 +10,11 @@ plt.plot(original_interval_10, label="original_interval_10")
 original_dependent_student = save_data_to_list(read_data("interval/original_dependent_student"))
 print("original_dependent_student: " + str(caculate_convergence_time(original_dependent_student)))
 plt.plot(original_dependent_student, label="original_dependent_student")
+
+# original,interval 1
+original_interval_1 = save_data_to_list(read_data("interval/original_interval_1"))
+print("original_interval_1: " + str(caculate_convergence_time(original_interval_1)))
+plt.plot(original_interval_1, label="original_interval_1")
 
 plt.legend()
 plt.show()
