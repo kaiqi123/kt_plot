18cdf31dd12cb1082b7648091dd11ee04b7e616e
kaiqi123 <kzhao27@asu.edu>
2019-01-23 12:50:38 -0700

    0104

--- cifar10_dependentStudent_0.001_plot.py
+++ cifar10_dependentStudent_0.001_plot.py
@@ -14,7 +14,7 @@ path="cifar10_results/Dependent_student_learning_rate_0.001/"
 #dependent_student_conv2_fc1
 dependent_student_conv2_fc1 = save_data_to_list(read_data(path+"conv2_fc1"))
 print("dependent_student_conv2_fc1: " + str(caculate_convergence_time(dependent_student_conv2_fc1)))
-#plt.plot(dependent_student_conv2_fc1, label="dependent_student_conv2_fc1")
+plt.plot(dependent_student_conv2_fc1, label="dependent_student_conv2_fc1")
 
 #dependent_student_conv3_fc1
 dependent_student_conv3_fc1 = save_data_to_list(read_data(path+"conv3_fc1"))
@@ -24,12 +24,12 @@ print("dependent_student_conv3_fc1: " + str(caculate_convergence_time(dependent_
 #dependent_student_conv4_fc1
 dependent_student_conv4_fc1 = save_data_to_list(read_data(path+"conv4_fc1"))
 print("dependent_student_conv4_fc1: " + str(caculate_convergence_time(dependent_student_conv4_fc1)))
-plt.plot(dependent_student_conv4_fc1, label="dependent_student_conv4_fc1")
+#plt.plot(dependent_student_conv4_fc1, label="dependent_student_conv4_fc1")
 
 #dependent_student_conv4_fc1, new method
 dependent_student_conv4_fc1 = save_data_to_list(read_data(path+"conv4_fc1_newMethod"))
 print("dependent_student_conv4_fc1: " + str(caculate_convergence_time(dependent_student_conv4_fc1)))
-plt.plot(dependent_student_conv4_fc1, label="dependent_student_conv4_fc1(new method)")
+#plt.plot(dependent_student_conv4_fc1, label="dependent_student_conv4_fc1(new method)")
 
 #independent_student
 path="cifar10_results/indepentStudent_lerning_rate_0.001/"
@@ -37,7 +37,7 @@ path="cifar10_results/indepentStudent_lerning_rate_0.001/"
 #independent_student_conv2_fc1
 independent_student_conv2_fc1_new = save_data_to_list(read_data(path+"independent_student_conv2_fc1_0.001"))
 print("independent_student_conv2_fc1_new: " + str(caculate_convergence_time(independent_student_conv2_fc1_new)))
-#plt.plot(independent_student_conv2_fc1_new, label="independent_student_conv2_fc1")
+plt.plot(independent_student_conv2_fc1_new, label="independent_student_conv2_fc1")
 
 #independent_student_conv3_fc1
 independent_student_conv3_fc1_new = save_data_to_list(read_data(path+"independent_student_conv3_fc1_0.001"))
@@ -47,7 +47,8 @@ print("independent_student_conv3_fc1_new: " + str(caculate_convergence_time(inde
 #independent_student_conv4_fc1
 independent_student_conv4_fc1_new = save_data_to_list(read_data(path+"independent_student_conv4_fc1_0.001"))
 print("independent_student_conv4_fc1_new: " + str(caculate_convergence_time(independent_student_conv4_fc1_new)))
-plt.plot(independent_student_conv4_fc1_new, label="independent_student_conv4_fc1")
+#plt.plot(independent_student_conv4_fc1_new, label="independent_student_conv4_fc1")
+
 
 plt.title("Cifar10: Dependent Student")
 plt.xlabel("epoch")
